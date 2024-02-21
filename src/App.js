cbd5a35092a69fac06acbe3e5121234f06ae7479
Franklin <franklin.op@hotmail.com>
2024-02-21 17:51:49 +0100

    connecting to blockchain to show categories

--- src/App.js
+++ src/App.js
@@ -13,10 +13,41 @@ import Dappazon from './abis/Dappazon.json'
 import config from './config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [dappazon, setDappazon] = useState(null)
   const [account, setAccount] = useState(null)
 
+  const [electronics, setElectronics] = useState(null)
+  const [clothing, setClothing] = useState(null)
+  const [toys, setToys] = useState(null)
 
-  const loadBlockchainData = async ()=>{
+
+  const loadBlockchainData = async () => {
+    //Connect to blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    //Connect to smart contracts
+    const dappazon = new ethers.Contract(
+      config[network.chainId].dappazon.address,
+      Dappazon,
+      provider)
+    
+    setDappazon(dappazon)
+    //Load Products
+
+    const items = []
+
+    for (var i = 0; i < 9; i++){
+      const item = await dappazon.items(i + 1)
+      items.push(item)
+    }
+
+    const electronics = items.filter((item) => item.category === 'electronics')
+    const clothing = items.filter((item) => item.category === 'clothing')
+    const toys = items.filter((item)=> item.category === 'toys')
     
   }
 
@@ -28,7 +59,7 @@ function App() {
     <div>
       <Navigation account= {account} setAccount={setAccount} />
 
-      <h2>Welcome to Dappazon</h2>
+      <h2>Dappazon Best Sellers</h2>
       
 
     </div>
